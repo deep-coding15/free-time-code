5240d24657609d35c2494cf75ab4a4b3a3f2e97b
deep-coding15 <tsafackmerveillem@gmail.com>
2026-06-06 23:05:19 +0100

    feat(project): Project Library done !

---
+++ javascript/3-Project-Library.js
@@ -0,0 +1,21 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this._id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+function getAllBooks() {
+    return myLibrary;
+}
